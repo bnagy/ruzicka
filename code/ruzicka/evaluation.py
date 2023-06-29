4150d1780181d4836504701f2b9e141a364061ab
ben <git@ben.iagu.net>
2023-06-29 19:01:40 +0200

    type type type

--- code/ruzicka/evaluation.py
+++ code/ruzicka/evaluation.py
@@ -14,19 +14,10 @@ See for more details:
     Task at PAN 2014. CLEF (Working Notes) 2014: 877-897.
 """
 
-from __future__ import print_function
-from itertools import combinations
-
-import seaborn as sb
-import numpy as np
-from scipy.cluster.hierarchy import dendrogram, linkage
-from sklearn.metrics import precision_recall_curve
 from sklearn.metrics import roc_auc_score
-from sklearn.metrics import average_precision_score as ap
-from sklearn.metrics import recall_score, precision_score, f1_score
+from typing import Collection
 
-
-def accuracy(prediction_scores, ground_truth_scores):
+def accuracy(prediction_scores, ground_truth_scores: Collection[float]) -> float:
     """
     Calculates the verification accuracy, assuming that every
     `score >= 0.5` represents an attribution.
@@ -58,12 +49,12 @@ def accuracy(prediction_scores, ground_truth_scores):
     assert len(ground_truth_scores) == len(prediction_scores)
 
     for gt_score, pred_score in zip(ground_truth_scores, prediction_scores):
-        if (pred_score > 0.5) == (gt_score > 0.5):
+        if (pred_score >= 0.5) == (gt_score >= 0.5):
             acc += 1.0
     return acc / float(len(prediction_scores))
 
 
-def auc(prediction_scores, ground_truth_scores):
+def auc(prediction_scores, ground_truth_scores: Collection[float]) -> float:
     """
     Calculates the AUC score (Area Under the Curve), a well-known
     scalar evaluation score for binary classifiers. This score
@@ -74,7 +65,7 @@ def auc(prediction_scores, ground_truth_scores):
     prediction_scores : array [n_problems]
 
         The predictions outputted by a verification system.
-        Assumes `0 >= prediction <=1`.
+        Assumes `0 <= prediction <=1`.
 
     ground_truth_scores : array [n_problems]
 
@@ -92,10 +83,10 @@ def auc(prediction_scores, ground_truth_scores):
 
     """
 
-    return roc_auc_score(ground_truth_scores, prediction_scores)
+    return float(roc_auc_score(ground_truth_scores, prediction_scores))
 
 
-def c_at_1(prediction_scores, ground_truth_scores):
+def c_at_1(prediction_scores, ground_truth_scores: Collection[float]) -> float:
     """
     Calculates the c@1 score, an evaluation method specific to the
     PAN competition. This method rewards predictions which leave
@@ -137,13 +128,15 @@ def c_at_1(prediction_scores, ground_truth_scores):
     nc, nu = 0.0, 0.0
     for gt_score, pred_score in zip(ground_truth_scores, prediction_scores):
         if pred_score == 0.5:
-            nu += 1
+            nu += 1.0
         elif (pred_score > 0.5) == (gt_score > 0.5):
             nc += 1.0
     return (1 / n) * (nc + (nu * nc / n))
 
 
-def pan_metrics(prediction_scores, ground_truth_scores):
+def pan_metrics(
+    prediction_scores, ground_truth_scores: Collection[float]
+) -> tuple[float, float, float]:
     """
     Convenience function: calculates all three PAN evaluation measures for the
     given `prediction_scores` and `ground_truth_scores`.
@@ -168,9 +161,11 @@ def pan_metrics(prediction_scores, ground_truth_scores):
     ----------
         E. Stamatatos, et al. Overview of the Author Identification
         Task at PAN 2014. CLEF (Working Notes) 2014: 877-897.
-    
+
     """
 
-    return accuracy(prediction_scores, ground_truth_scores), \
-           auc(prediction_scores, ground_truth_scores), \
-           c_at_1(prediction_scores, ground_truth_scores)
+    return (
+        accuracy(prediction_scores, ground_truth_scores),
+        auc(prediction_scores, ground_truth_scores),
+        c_at_1(prediction_scores, ground_truth_scores),
+    )
